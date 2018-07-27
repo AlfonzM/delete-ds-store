ae7f8f719540f4b646cbba009fa649025b619cce
Alfonz Montelibano <m.alfonz@gmail.com>
2018-07-27 11:02:28 +0800

    Update commands and logs. 0.1.1

--- README.md
+++ README.md
@@ -1,13 +1,44 @@
 # delete-ds-store
 
-> Removes all .DS_Store in the current directory recursively
+> Removes all .DS_Store files in the current directory recursively
+
+💩 Turn this...
+
+```
+my-project/
+--| .DS_Store
+--| src/
+-----| .DS_Store
+-----| index.html
+-----| js
+-------| .DS_Store
+-------| app.js
+```
+
+✨ ...into this
+
+```
+my-project/
+--| src/
+-----| index.html
+-----| js
+-------| app.js
+```
 
 ## Installation
 
 Use it as a global command.
 
+**npm**
+
 ```
-$ npm install --global delete-ds-store
+$ npm install -g delete-ds-store
+```
+
+**yarn**
+
+```
+$ yarn global add delete-ds-store
 ```
 
 ## Usage
--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 #! /usr/bin/env node
+var shell = require('shelljs');
+var colors = require('colors');
+var emoji = require('node-emoji');
 
-var shell = require("shelljs");
+var FIND_DS_STORE_COMMAND = 'find . -name "*.DS_Store"';
+var DELETE_DS_STORE_COMMAND = FIND_DS_STORE_COMMAND + ' -type f -delete';
+var COUNT_DS_STORE_COMMAND = FIND_DS_STORE_COMMAND + ' | wc -l';
 
-shell.exec("echo Deleting all .DS_Stores recursively...");
-shell.exec("rm -rf **/.DS_Store");
-shell.exec("echo Done!");
+console.log();
+console.log(emoji.emojify(':rocket:  Deleting all .DS_Stores recursively...'.yellow));
+
+// Count DS_Stores to be deleted
+var count = shell.exec(COUNT_DS_STORE_COMMAND, { silent: true });
+
+// Delete DS_Stores
+shell.exec(DELETE_DS_STORE_COMMAND);
+
+console.log(emoji.emojify(':fire:  ' + Number(count) + ' DS_Stores deleted').green);
+console.log(emoji.emojify(':sparkles:  Done!'));
